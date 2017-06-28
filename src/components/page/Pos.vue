6a75b6f5971bdee48cbac8f648b7256a0c2a1529
paranoidyang <paranoidyang@163.com>
2017-06-28 12:04:43 +0800

    添加商品动态增加数量和价格

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -5,17 +5,20 @@
 			<el-col :span='7' class="pos_order" id="order_list">
 				<el-tabs>
 					<el-tab-pane label="点餐">
-						<el-table :data="tableData" border show-summary style="width: 100%" >
+						<el-table :data="tableData" border  style="width: 100%" >
 							<el-table-column prop="goodsName" label="商品"  ></el-table-column>
-							<el-table-column prop="count" label="量" width="50"></el-table-column>
-							<el-table-column prop="price" label="金额" width="70"></el-table-column>
+							<el-table-column prop="count" label="数量" width="70"></el-table-column>
+							<el-table-column prop="price" label="单价"width="70"></el-table-column>
 							<el-table-column  label="操作" width="100" fixed="right">
 							<template scope="scope">
 								<el-button type="text" size="small">删除</el-button>
-								<el-button type="text" size="small">增加</el-button>
+								<el-button type="text" size="small" @click="addOrderList(scope.row)">增加</el-button>
 							</template>
 							</el-table-column>
 						</el-table>
+						<div class="totalDiv">
+							<small>数量：</small>{{totalCount}} &nbsp;&nbsp&nbsp;&nbsp;<small>金额：</small>{{totalMoney}}元
+						</div>
 
 					<div class="div_btn">
 						<el-button type="warning" >挂单</el-button>
@@ -39,7 +42,7 @@
 		    <div class="title">常用商品</div>
 		    	<div class="often-goods-list">
 			        <ul>
-			            <li v-for="goods in oftenGoods">
+			            <li v-for="goods in oftenGoods" @click="addOrderList(goods)">
 			                <span>{{goods.goodsName}}</span>
 			                <span class="o-price">￥{{goods.price}}元</span>
 			            </li>
@@ -52,7 +55,7 @@
     <el-tabs>
         <el-tab-pane label="汉堡">
             <ul class='cookList'>
-    			<li v-for="goods in type0Goods">
+    			<li v-for="goods in type0Goods" @click="addOrderList(goods)">
 				    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
 				    <span class="foodName">{{goods.goodsName}}</span>
 				    <span class="foodPrice">￥{{goods.price}}元</span>
@@ -62,7 +65,7 @@
 
          <el-tab-pane label="小食">
              <ul class='cookList'>
-    			<li v-for="goods in type1Goods">
+    			<li v-for="goods in type1Goods" @click="addOrderList(goods)">
 				    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
 				    <span class="foodName">{{goods.goodsName}}</span>
 				    <span class="foodPrice">￥{{goods.price}}元</span>
@@ -72,17 +75,17 @@
 
         <el-tab-pane label="饮料">
              <ul class='cookList'>
-    			<li v-for="goods in type2Goods">
+    			<li v-for="goods in type2Goods" @click="addOrderList(goods)">
 				    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
 				    <span class="foodName">{{goods.goodsName}}</span>
 				    <span class="foodPrice">￥{{goods.price}}元</span>
 				</li>
 			</ul>
         </el-tab-pane>
-        
+
         <el-tab-pane label="套餐">
              <ul class='cookList'>
-    			<li v-for="goods in type3Goods">
+    			<li v-for="goods in type3Goods" @click="addOrderList(goods)">
 				    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
 				    <span class="foodName">{{goods.goodsName}}</span>
 				    <span class="foodPrice">￥{{goods.price}}元</span>
@@ -104,31 +107,15 @@ export default {
 	name: 'Pos',
 	data(){
 		return{
-			tableData: [{
-				goodsName: '可口可乐',
-				price: 8,
-				count:1
-				}, {
-
-				goodsName: '香辣鸡腿堡',
-				price: 15,
-				count:1
-				}, {
-
-				goodsName: '爱心薯条',
-				price: 8,
-				count:1
-				}, {
-
-				goodsName: '甜筒',
-				price: 8,
-				count:1
-				}],
+		   tableData:[],
 		   oftenGoods:[],
 	       type0Goods:[],
 	       type1Goods:[],
 	       type2Goods:[],
 	       type3Goods:[],
+	        totalCount:0, 
+            totalMoney:0,
+	      
 		}
 	},
 
@@ -160,12 +147,49 @@ export default {
       })
   },
 
+methods:{
+      //添加订单列表的方法
+      addOrderList(goods){
+      		 this.totalCount=0; //汇总数量清0
+            this.totalMoney=0;
+            let isHave=false;
+            //判断是否这个商品已经存在于订单列表
+            for (let i=0; i<this.tableData.length;i++){
+                console.log(this.tableData[i].goodsId);
+                if(this.tableData[i].goodsId==goods.goodsId){
+                    isHave=true; //存在
+                }
+            }
+            //根据isHave的值判断订单列表中是否已经有此商品
+            if(isHave){
+                //存在就进行数量添加
+                 let arr = this.tableData.filter(o =>o.goodsId == goods.goodsId);
+                 arr[0].count++;
+
+                 //console.log(arr);
+            }else{
+                //不存在就推入数组
+                let newGoods={goodsId:goods.goodsId,goodsName:goods.goodsName,price:goods.price,count:1};
+                 this.tableData.push(newGoods);
+ 
+            }
+ 
+            //进行数量和价格的汇总计算
+            this.tableData.forEach((element) => {
+                this.totalCount+=element.count;
+                this.totalMoney+=element.price*element.count;   
+            });
+           
+      }
+  },
+
 	mounted:function(){
 	var orderHeight=document.body.clientHeight;
 	document.getElementById("order_list").style.height=orderHeight+'px';
 	},
 }
 </script>
+
 <style scoped>
 	.pos_order{
 		background-color: #f9fafc;
@@ -189,6 +213,7 @@ export default {
       padding:10px;
       margin:5px;
       background-color:#fff;
+      cursor: pointer;
    }
   .o-price{
       color:#58B7FF; 
@@ -208,7 +233,7 @@ export default {
        padding: 2px;
        float:left;
        margin: 2px;
- 
+ 	   cursor: pointer;
    }
    .cookList li span{
        
@@ -219,7 +244,7 @@ export default {
        width: 40%;
    }
    .foodName{
-       font-size: 18px;
+       font-size: 16px;
        padding-left: 10px;
        color:brown;
  
@@ -228,5 +253,10 @@ export default {
        font-size: 16px;
        padding-left: 10px;
        padding-top:10px;
+   }
+   .totalDiv{
+   	background-color: #fff;
+   	padding: 10px;
+   	border-bottom: 1px solid #E5E9F2;
    }
 </style>
